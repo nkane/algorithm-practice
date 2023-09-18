be970d6a67c013497c8bebf8c1641baf6814a1e8
Nick Kane <nkanedev@gmail.com>
2023-09-17 22:39:03 -0400

    update link list and related test for go

--- kata-machine-go/src/data-structures/singly_linked_list.go
+++ kata-machine-go/src/data-structures/singly_linked_list.go
@@ -1,5 +1,10 @@
 package data_structures
 
+import (
+	"errors"
+	"log"
+)
+
 type Node struct {
 	Next  *Node       `json:"next"`
 	Value interface{} `json:"value"`
@@ -9,4 +14,62 @@ type SinglyLinkedList struct {
 	Head   *Node `json:"head"`
 	Tail   *Node `json:"tail"`
 	Length int   `json:"length"`
+}
+
+func (l *SinglyLinkedList) Prepend(v interface{}) error {
+	if l.Length == 0 {
+		return l.Append(v)
+	}
+	return nil
+}
+
+func (l *SinglyLinkedList) Append(v interface{}) error {
+	n := &Node{
+		Next:  nil,
+		Value: v,
+	}
+	if l.Length == 0 {
+		l.Head = n
+		l.Tail = n
+	} else {
+		l.Tail.Next = n
+		l.Tail = n
+	}
+	l.Length++
+	return nil
+}
+
+func (l *SinglyLinkedList) Insert(v interface{}, idx int) (*Node, error) {
+	return nil, nil
+}
+
+func (l *SinglyLinkedList) Remove(idx int) interface{} {
+	return nil
+}
+
+func (l *SinglyLinkedList) Get(idx int) (*Node, error) {
+	if (idx > (l.Length - 1)) || (idx < 0) {
+		return nil, errors.New("index out of range")
+	}
+	n := l.Head
+	for i := 0; i < idx; i++ {
+		n = n.Next
+		if n == nil {
+			return nil, errors.New("node is nil")
+		}
+	}
+	return n, nil
+}
+
+func (l *SinglyLinkedList) RemoveAt(idx int) interface{} {
+	return nil
+}
+
+func (l *SinglyLinkedList) DumpInfo() {
+	n := l.Head
+	log.Printf("List Length: %d\n", l.Length)
+	for n != nil {
+		log.Printf("%p - %+v\n", n, n)
+		n = n.Next
+	}
 }
--- kata-machine-go/src/data-structures/singly_linked_list_test.go
+++ kata-machine-go/src/data-structures/singly_linked_list_test.go
@@ -3,6 +3,24 @@ package data_structures
 import "testing"
 
 func TestSinglyLinkedList(t *testing.T) {
-	list := SinglyLinkedList{}
-	t.Logf("list %+v", list)
+	l := SinglyLinkedList{}
+	if l.Length != 0 {
+		t.Fatalf("expected list length to be zero")
+	}
+	l.Append(1)
+	if l.Length != 1 {
+		t.Fatalf("expected list length to be one")
+	}
+	if l.Head != l.Tail {
+		t.Fatalf("expected head and tail to be equal")
+	}
+	l.Append(2)
+	if l.Length != 2 {
+		t.Fatalf("expected length of list to be two")
+	}
+	if l.Head == l.Tail {
+		t.Fatalf("expected list head and tail to be different")
+	}
+	// TODO(nick): add get test
+	l.DumpInfo()
 }
