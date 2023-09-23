04738005f6c9374448fe8b82f08c37003a513a49
Nick Kane <nkanedev@gmail.com>
2023-09-23 16:56:52 -0400

    adding quick find compression example

--- algorithms-part-1/week-1/algorithms/union_find.go
+++ algorithms-part-1/week-1/algorithms/union_find.go
@@ -12,9 +12,10 @@ import (
 type UnionDataType string
 
 var (
-	QuickFindType             = UnionDataType("quick_find")
-	QuickUnionFindType        = UnionDataType("quick_union_find")
-	WeightedQuickUionFindType = UnionDataType("weighted_quick_union_find")
+	QuickFindType              = UnionDataType("quick_find")
+	QuickUnionFindType         = UnionDataType("quick_union_find")
+	WeightedQuickUnionFindType = UnionDataType("weighted_quick_union_find")
+	QuickFindCompressedType    = UnionDataType("quick_find_compressed")
 )
 
 type UnionFind interface {
@@ -25,6 +26,10 @@ type UnionFind interface {
 	GetIDs() []int
 }
 
+// QuickFind
+// constructor: O(N)
+// union: O(N)
+// find: O(1)
 type QuickFind struct {
 	IDs   []int
 	Count int
@@ -74,6 +79,10 @@ func CreateQuickFind(n int) UnionFind {
 	return &qf
 }
 
+// QuickUnionFind
+// constructor: O(N)
+// union: tree height
+// find: tree height
 type QuickUnionFind struct {
 	IDs   []int
 	Count int
@@ -122,6 +131,124 @@ func CreateQuickUnionFind(n int) UnionFind {
 	return &uf
 }
 
+// WeightedQuickUnionFind
+// constructor: O(N)
+// union: O(lg N)
+// find: O(lg N)
+type WeightedQuickUnionFind struct {
+	IDs   []int
+	Size  []int
+	Count int
+}
+
+func (wf *WeightedQuickUnionFind) Connected(p int, q int) bool {
+	pID := wf.Find(p)
+	qID := wf.Find(q)
+	return pID == qID
+}
+
+func (wf *WeightedQuickUnionFind) Find(p int) int {
+	for p != wf.IDs[p] {
+		p = wf.IDs[p]
+	}
+	return p
+}
+
+func (wf *WeightedQuickUnionFind) Union(p int, q int) {
+	pID := wf.Find(p)
+	qID := wf.Find(q)
+	if pID == qID {
+		return
+	}
+	if wf.Size[pID] < wf.Size[qID] {
+		wf.IDs[pID] = qID
+		wf.Size[qID] += wf.Size[pID]
+	} else {
+		wf.IDs[qID] = pID
+		wf.Size[pID] += wf.Size[qID]
+	}
+	wf.Count--
+}
+
+func (wf *WeightedQuickUnionFind) GetCount() int {
+	return wf.Count
+}
+
+func (wf *WeightedQuickUnionFind) GetIDs() []int {
+	return wf.IDs
+}
+
+func CreateWeightedQuickUnionFind(n int) UnionFind {
+	wf := WeightedQuickUnionFind{
+		Count: n, IDs: make([]int, n),
+		Size: make([]int, n),
+	}
+	for i := 0; i < n; i++ {
+		wf.IDs[i] = i
+		wf.Size[i] = 1
+	}
+	return &wf
+}
+
+type QuickFindPathCompression struct {
+	IDs   []int
+	Count int
+}
+
+func (qf *QuickFindPathCompression) Connected(p int, q int) bool {
+	pID := qf.Find(p)
+	qID := qf.Find(q)
+	return pID == qID
+}
+
+func (qf *QuickFindPathCompression) Find(p int) int {
+	root := p
+	for qf.IDs[root] != root {
+		root = qf.IDs[root]
+	}
+	// path compression
+	for p != root {
+		next := qf.IDs[p]
+		qf.IDs[p] = root
+		p = next
+	}
+	return root
+}
+
+func (qf *QuickFindPathCompression) Union(p int, q int) {
+	// put p and q into the same component
+	pID := qf.Find(p)
+	qID := qf.Find(q)
+	if pID == qID {
+		return
+	}
+	for i := 0; i < len(qf.IDs); i++ {
+		if qf.IDs[i] == pID {
+			qf.IDs[i] = qID
+		}
+	}
+	qf.Count--
+}
+
+func (qf *QuickFindPathCompression) GetCount() int {
+	return qf.Count
+}
+
+func (qf *QuickFindPathCompression) GetIDs() []int {
+	return qf.IDs
+}
+
+func CreateQuickFindPathCompression(n int) UnionFind {
+	qf := QuickFindPathCompression{
+		Count: n,
+		IDs:   make([]int, n),
+	}
+	for i := 0; i < n; i++ {
+		qf.IDs[i] = i
+	}
+	return &qf
+}
+
 func CreateUnionFindFromFile(path string, t UnionDataType) (UnionFind, error) {
 	rf, err := os.Open(path)
 	if err != nil {
@@ -144,6 +271,10 @@ func CreateUnionFindFromFile(path string, t UnionDataType) (UnionFind, error) {
 		uf = CreateQuickUnionFind(n)
 	case QuickFindType:
 		uf = CreateQuickFind(n)
+	case WeightedQuickUnionFindType:
+		uf = CreateWeightedQuickUnionFind(n)
+	case QuickFindCompressedType:
+		uf = CreateQuickFindPathCompression(n)
 	}
 	for fs.Scan() {
 		line := fs.Text()
--- algorithms-part-1/week-1/algorithms/union_find_test.go
+++ algorithms-part-1/week-1/algorithms/union_find_test.go
@@ -15,6 +15,11 @@ func TestQuickFindTiny(t *testing.T) {
 	if len(ids) != len(expectedOutput) {
 		t.Fatalf("expected lengths do not match, got %d", len(ids))
 	}
+	for i := 0; i < len(ids); i++ {
+		if ids[i] != expectedOutput[i] {
+			t.Fatalf("failed comparison check at index %d", i)
+		}
+	}
 }
 
 func TestQuickUnionFindTiny(t *testing.T) {
@@ -35,4 +40,30 @@ func TestQuickUnionFindTiny(t *testing.T) {
 			t.Fatalf("failed comparison check at index %d", i)
 		}
 	}
+}
+
+func TestWeightedQuickUnionFind(t *testing.T) {
+	expectedOutput := []int{6, 2, 6, 4, 4, 6, 6, 2, 4, 4}
+	uf, err := CreateUnionFindFromFile("./data/tinyUF.txt", WeightedQuickUnionFindType)
+	if err != nil {
+		t.Fatalf("expected error to be nil, received error: %+v", err)
+	}
+	if uf.GetCount() != 2 {
+		t.Fatalf("expected initial count to be 2, got %d", uf.GetCount())
+	}
+	ids := uf.GetIDs()
+	if len(ids) != len(expectedOutput) {
+		t.Fatalf("expected lengths do not match, got %d", len(ids))
+	}
+	for i := 0; i < len(ids); i++ {
+		if ids[i] != expectedOutput[i] {
+			t.Fatalf("failed comparison check at index %d", i)
+		}
+	}
+}
+
+func TestQuickFindCompressionTiny(t *testing.T) {
+	uf := CreateQuickFindPathCompression(10)
+	uf.Union(1, 2)
+	uf.Union(1, 3)
 }
