43f970fc399435158a10f0b8fe44a80ffae3af3c
Nick Kane <nkanedev@gmail.com>
2023-09-24 16:26:06 -0400

    update drawing logic for percolation visualization

--- algorithms-part-1/week-1/algorithms/percolation/go.mod
+++ algorithms-part-1/week-1/algorithms/percolation/go.mod
@@ -2,4 +2,7 @@ module ray-lib
 
 go 1.21.1
 
-require github.com/gen2brain/raylib-go/raylib v0.0.0-20230826160040-f770ca098119 // indirect
+require (
+	github.com/gen2brain/raylib-go/raygui v0.0.0-20230826160040-f770ca098119 // indirect
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20230826160040-f770ca098119 // indirect
+)
--- algorithms-part-1/week-1/algorithms/percolation/main.go
+++ algorithms-part-1/week-1/algorithms/percolation/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"log"
+
+	gui "github.com/gen2brain/raylib-go/raygui"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 const (
-	DefaultRows    = int32(20)
-	DefaultColumns = int32(20)
+	DefaultFPS = int32(60)
 )
 
 var (
 	ScreenWidth  = int32(1280)
 	ScreenHeight = int32(720)
-	CellWidth    = int32(ScreenWidth / DefaultColumns)
-	CellHeight   = int32(ScreenHeight / DefaultRows)
 )
 
 type Window struct {
@@ -28,46 +28,84 @@ type Cell struct {
 	Open bool
 }
 
-func (c *Cell) Draw() {
-	if c.Open {
-		rl.DrawRectangle(int32(c.X)*CellWidth, int32(c.Y)*CellHeight, CellWidth, CellHeight, rl.White)
-	} else {
-		rl.DrawRectangle(int32(c.X)*CellWidth, int32(c.Y)*CellHeight, CellWidth, CellHeight, rl.Black)
+type State struct {
+	CellWidth  int32
+	CellHeight int32
+}
+
+type Game struct {
+	Window *Window
+	State  State
+	Grid   [][]Cell
+}
+
+func CreateGame(n int32, w *Window) *Game {
+	g := Game{
+		Window: w,
+		State:  State{},
 	}
-	rl.DrawRectangleLines(int32(c.X)*CellWidth, int32(c.Y)*CellHeight, CellWidth, CellHeight, rl.Black)
+	g.InitializeGrid(n)
+	return &g
+}
+
+func (g *Game) InitializeGrid(n int32) {
+	g.State.CellWidth = (g.Window.Width / 2) / n
+	g.State.CellHeight = g.Window.Height / n
+	g.Grid = make([][]Cell, n)
+	for column := 0; column < len(g.Grid); column++ {
+		g.Grid[column] = make([]Cell, n)
+		for row := 0; row < len(g.Grid[column]); row++ {
+			g.Grid[column][row] = Cell{
+				X:    column,
+				Y:    row,
+				Open: true,
+			}
+		}
+	}
+}
+
+func (g *Game) Update() {
+	// TODO
+}
+
+func (g *Game) Render() {
+	for column := 0; column < len(g.Grid); column++ {
+		for row := 0; row < len(g.Grid[column]); row++ {
+			g.Grid[column][row].Draw(g.State.CellWidth, g.State.CellHeight, g.Window.Width/2)
+		}
+	}
+}
+
+func (c *Cell) Draw(cellWidth int32, cellHeight int32, xOffset int32) {
+	if c.Open {
+		rl.DrawRectangle(int32(c.X)*cellWidth+xOffset, int32(c.Y)*cellHeight, cellWidth, cellHeight, rl.White)
+	} else {
+		rl.DrawRectangle(int32(c.X)*cellWidth+xOffset, int32(c.Y)*cellHeight, cellWidth, cellHeight, rl.Black)
+	}
+	rl.DrawRectangleLines(int32(c.X)*cellWidth+xOffset, int32(c.Y)*cellHeight, cellWidth, cellHeight, rl.Black)
 }
 
 func main() {
 	w := Window{
 		Width:  ScreenWidth,
 		Height: ScreenHeight,
-		FPS:    60,
+		FPS:    DefaultFPS,
 	}
 	rl.InitWindow(w.Width, w.Height, "percolation simulation")
 	rl.SetTargetFPS(w.FPS)
 
-	columns := int(DefaultColumns)
-	rows := int(DefaultRows)
-
-	grid := [DefaultColumns][DefaultRows]Cell{}
-
-	for i := 0; i < columns; i++ {
-		for j := 0; j < rows; j++ {
-			grid[i][j] = Cell{
-				X: i,
-				Y: j,
-			}
-		}
-	}
+	n := int32(5)
+	game := CreateGame(n, &w)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		{
 			rl.ClearBackground(rl.RayWhite)
-			for i := 0; i < columns; i++ {
-				for j := 0; j < rows; j++ {
-					grid[i][j].Draw()
-				}
+			game.Update()
+			game.Render()
+			if gui.Spinner(rl.NewRectangle(50, 10, 150, 25), "N Value", &n, 5, 20, false) {
+				log.Printf("reinit grid\n")
+				game.InitializeGrid(n)
 			}
 		}
 		rl.EndDrawing()
