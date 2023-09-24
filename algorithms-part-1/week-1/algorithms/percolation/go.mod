ea30ebe0353e9bac49ba6bfc00be8868aaa492b5
Nick Kane <nkanedev@gmail.com>
2023-09-24 13:26:22 -0400

    updated percolation visualization to use raylib

--- algorithms-part-1/week-1/algorithms/percolation/go.mod
+++ algorithms-part-1/week-1/algorithms/percolation/go.mod
@@ -1,10 +1,5 @@
-module percolation
+module ray-lib
 
 go 1.21.1
 
-require (
-	github.com/fogleman/gg v1.3.0 // indirect
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/shomali11/gridder v0.0.0-20210930173142-5f3b82d74585 // indirect
-	golang.org/x/image v0.0.0-20200119044424-58c23975cae1 // indirect
-)
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20230826160040-f770ca098119 // indirect
--- algorithms-part-1/week-1/algorithms/percolation/main.go
+++ algorithms-part-1/week-1/algorithms/percolation/main.go
@@ -1,30 +1,77 @@
 package main
 
 import (
-	"image/color"
-	"log"
-
-	"github.com/shomali11/gridder"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+const (
+	DefaultRows    = int32(20)
+	DefaultColumns = int32(20)
+)
+
+var (
+	ScreenWidth  = int32(1280)
+	ScreenHeight = int32(720)
+	CellWidth    = int32(ScreenWidth / DefaultColumns)
+	CellHeight   = int32(ScreenHeight / DefaultRows)
+)
+
+type Window struct {
+	Width  int32
+	Height int32
+	FPS    int32
+}
+
+type Cell struct {
+	X    int
+	Y    int
+	Open bool
+}
+
+func (c *Cell) Draw() {
+	if c.Open {
+		rl.DrawRectangle(int32(c.X)*CellWidth, int32(c.Y)*CellHeight, CellWidth, CellHeight, rl.White)
+	} else {
+		rl.DrawRectangle(int32(c.X)*CellWidth, int32(c.Y)*CellHeight, CellWidth, CellHeight, rl.Black)
+	}
+	rl.DrawRectangleLines(int32(c.X)*CellWidth, int32(c.Y)*CellHeight, CellWidth, CellHeight, rl.Black)
+}
+
 func main() {
-	imgConfig := gridder.ImageConfig{
-		Name: "grid.png",
+	w := Window{
+		Width:  ScreenWidth,
+		Height: ScreenHeight,
+		FPS:    60,
 	}
-	gridConfig := gridder.GridConfig{
-		Rows:              20,
-		Columns:           20,
-		LineStrokeWidth:   2,
-		BorderStrokeWidth: 10,
-		LineColor:         color.Black,
-		BorderColor:       color.Black,
-		BackgroundColor:   color.White,
+	rl.InitWindow(w.Width, w.Height, "percolation simulation")
+	rl.SetTargetFPS(w.FPS)
+
+	columns := int(DefaultColumns)
+	rows := int(DefaultRows)
+
+	grid := [DefaultColumns][DefaultRows]Cell{}
+
+	for i := 0; i < columns; i++ {
+		for j := 0; j < rows; j++ {
+			grid[i][j] = Cell{
+				X: i,
+				Y: j,
+			}
+		}
 	}
-	grid, err := gridder.New(imgConfig, gridConfig)
-	if err != nil {
-		log.Fatal(err)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		{
+			rl.ClearBackground(rl.RayWhite)
+			for i := 0; i < columns; i++ {
+				for j := 0; j < rows; j++ {
+					grid[i][j].Draw()
+				}
+			}
+		}
+		rl.EndDrawing()
 	}
-	if err := grid.SavePNG(); err != nil {
-		log.Fatal(err)
-	}
+
+	rl.CloseWindow()
 }
