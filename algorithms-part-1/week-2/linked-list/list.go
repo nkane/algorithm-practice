a56845788a75012850c6bd53688b5d927f02f7f9
Nick Kane <nkanedev@gmail.com>
2023-10-24 09:19:36 -0400

    update singly linked list and updating test

--- algorithms-part-1/week-2/linked-list/list.go
+++ algorithms-part-1/week-2/linked-list/list.go
@@ -6,36 +6,45 @@ type Node struct {
 }
 
 type SingleLinkedList struct {
-	Head *Node
-	Tail *Node
+	Head        *Node
+	Tail        *Node
+	Length      int
+	IteratorIdx int
 }
 
-func CreateSingleLinkedList(n *Node) *SingleLinkedList {
-	list := SingleLinkedList{
-		Head: n,
-		Tail: n,
-	}
+func CreateSingleLinkedList() *SingleLinkedList {
+	list := SingleLinkedList{}
 	return &list
 }
 
-func (l *SingleLinkedList) Append(n *Node) {
+func (l *SingleLinkedList) Append(v interface{}) {
+	n := Node{
+		Next:  nil,
+		Value: v,
+	}
+	l.Length++
 	if l.Head == nil {
-		l.Head = n
-		l.Tail = n
+		l.Head = &n
+		l.Tail = &n
 		return
 	}
-	l.Tail.Next = n
-	l.Tail = n
+	l.Tail.Next = &n
+	l.Tail = &n
 }
 
-func (l *SingleLinkedList) Prepend(n *Node) {
+func (l *SingleLinkedList) Prepend(v interface{}) {
+	n := Node{
+		Next:  nil,
+		Value: v,
+	}
+	l.Length++
 	if l.Head == nil {
-		l.Head = n
-		l.Tail = n
+		l.Head = &n
+		l.Tail = &n
 		return
 	}
 	n.Next = l.Head
-	l.Head = n
+	l.Head = &n
 }
 
 func (l *SingleLinkedList) Find(idx int) *Node {
@@ -43,10 +52,24 @@ func (l *SingleLinkedList) Find(idx int) *Node {
 	sIdx := 0
 	for node != nil && sIdx < idx {
 		node = node.Next
+		sIdx++
 	}
 	return node
 }
 
 func (l *SingleLinkedList) RemoveAt(idx int) {
 	// TODO
+	l.Length--
+}
+
+func (l *SingleLinkedList) Next() *Node {
+	if l.Length < l.IteratorIdx {
+		return nil
+	}
+	node := l.Find(l.IteratorIdx)
+	if node == nil {
+		return nil
+	}
+	l.IteratorIdx++
+	return node
 }
--- algorithms-part-1/week-2/linked-list/list_test.go
+++ algorithms-part-1/week-2/linked-list/list_test.go
@@ -3,25 +3,48 @@ package list
 import "testing"
 
 func TestCreateList(t *testing.T) {
-	list := CreateSingleLinkedList(nil)
+	list := CreateSingleLinkedList()
 	if list == nil {
 		t.Fatalf("failed to create list")
 	}
 }
 
 func TestListSingleNode(t *testing.T) {
-	list := CreateSingleLinkedList(&Node{
-		Next:  nil,
-		Value: 32,
-	})
+	list := CreateSingleLinkedList()
 	if list == nil {
 		t.Fatalf("failed to create list")
 	}
+	list.Append(32)
 	node := list.Find(0)
 	if node == nil {
 		t.Fatalf("failed to find node in list at idx 0")
 	}
 	if node.Value != 32 {
 		t.Fatalf("failed node value comparison")
+	}
+}
+
+func TestListMultiNode(t *testing.T) {
+	list := CreateSingleLinkedList()
+	if list == nil {
+		t.Fatalf("failed to create list")
+	}
+	list.Append(32)
+	list.Append(64)
+	list.Append(128)
+	list.Prepend(16)
+	list.Prepend(8)
+	expectedOrder := []int{8, 16, 32, 64, 128}
+	actualOrder := []int{}
+	for node := list.Next(); node != nil; node = list.Next() {
+		actualOrder = append(actualOrder, node.Value.(int))
+	}
+	if len(expectedOrder) != len(actualOrder) {
+		t.Fatalf("expected order and actual order lengths do not match")
+	}
+	for idx, v := range expectedOrder {
+		if actualOrder[idx] != v {
+			t.Fatalf("idx: %d does not match expected value of %d", idx, v)
+		}
 	}
 }
