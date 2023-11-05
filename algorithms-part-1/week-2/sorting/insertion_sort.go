84211fdbac24bf4a4a397025bd88f6f9861c502a
Nick Kane <nkanedev@gmail.com>
2023-11-04 23:55:42 -0400

    setting up for tomorrow

---
+++ algorithms-part-1/week-2/sorting/insertion_sort.go
@@ -0,0 +1,8 @@
+package sort
+
+import "cmp"
+
+func InsertionSort[T cmp.Ordered](list []T) []T {
+	// TODO(nick): implement
+	return list
+}
---
+++ algorithms-part-1/week-2/sorting/shell_sort.go
@@ -0,0 +1,8 @@
+package sort
+
+import "cmp"
+
+func ShellSort[T cmp.Ordered](list []T) []T {
+	// TODO(nick): implement
+	return list
+}
