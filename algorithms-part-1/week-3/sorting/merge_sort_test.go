330b1778761d1964133baa72121ae0103b908a9d
Nick Kane <nkanedev@gmail.com>
2023-11-07 23:31:40 -0500

    update merge sort test

--- algorithms-part-1/week-3/sorting/merge_sort_test.go
+++ algorithms-part-1/week-3/sorting/merge_sort_test.go
@@ -17,3 +17,14 @@ func TestBasicMergeSort(t *testing.T) {
 
 	assert.Equal(t, expectedOutputString, testOutputString)
 }
+
+func TestMergeSort(t *testing.T) {
+	testInput := []int{2048, 128, 4, 32, 64, 1024, 256, 16, -256, 0, 8, 2, -128}
+	testOutput := MergeSort[int](testInput)
+	testOutputString := fmt.Sprint(testOutput)
+
+	expectedOutput := []int{-256, -128, 0, 2, 4, 8, 16, 32, 64, 128, 256, 1024, 2048}
+	expectedOutputString := fmt.Sprint(expectedOutput)
+
+	assert.Equal(t, expectedOutputString, testOutputString)
+}
