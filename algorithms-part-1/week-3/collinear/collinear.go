e6ac224b58d3269d7ec98a9f012363c8f39d94a4
Nick Kane <nkanedev@gmail.com>
2023-11-10 13:57:49 -0500

    adding collinear initial test

--- algorithms-part-1/week-3/collinear/collinear.go
+++ algorithms-part-1/week-3/collinear/collinear.go
@@ -1,14 +1,23 @@
 package collinear
 
 type Point struct {
-	X int
-	Y int
+	X float64
+	Y float64
 }
 
 type LineSegment struct {
-	P     Point
-	Q     Point
+	A     Point
+	B     Point
 	Slope float64
+}
+
+func NewLineSegment(a Point, b Point) LineSegment {
+	result := LineSegment{
+		A: a,
+		B: b,
+	}
+	result.Slope = (b.Y - a.Y) / (b.X - a.X)
+	return result
 }
 
 type CollinearReturnValue struct {
@@ -17,7 +26,18 @@ type CollinearReturnValue struct {
 }
 
 func BruteCollinearPoints(points []Point) CollinearReturnValue {
-	return CollinearReturnValue{}
+	result := CollinearReturnValue{}
+	for i := 0; i < len(points); i += 4 {
+		p := points[i+0]
+		q := points[i+1]
+		segmentA := NewLineSegment(p, q)
+		result.Segments = append(result.Segments, segmentA)
+		r := points[i+2]
+		s := points[i+3]
+		segmentB := NewLineSegment(r, s)
+		result.Segments = append(result.Segments, segmentB)
+	}
+	return result
 }
 
 func FastCollinearPoints(points []Point) CollinearReturnValue {
